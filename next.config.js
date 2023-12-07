f6e396926184ed36aa5e5df13004128e8f521f08
Zdravko Danailov <zdravkodanailov7@gmail.com>
2023-12-07 14:52:13 +0000

    Bug Fixes

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: { "unoptimized": true },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
