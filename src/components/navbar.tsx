8b54117dabcfadb909215480c9a0790df19a5d67
Zdravko Danailov <zdravkodanailov7@gmail.com>
2025-08-24 17:38:23 +0100

    added images, navbar links open new page now, update data

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -22,6 +22,7 @@ export default function Navbar() {
               <TooltipTrigger asChild>
                 <Link
                   href={item.href}
+                  target="_blank"
                   className={cn(
                     buttonVariants({ variant: "ghost", size: "icon" }),
                     "size-12"
@@ -45,6 +46,7 @@ export default function Navbar() {
                 <TooltipTrigger asChild>
                   <Link
                     href={social.url}
+                    target="_blank"
                     className={cn(
                       buttonVariants({ variant: "ghost", size: "icon" }),
                       "size-12"
