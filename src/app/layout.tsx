8881e61fa9657538cf62b627fdca59b0bb6c69ec
Zdravko Danailov <zdravkodanailov7@gmail.com>
2025-09-12 22:15:56 +0100

    Move the font variable to html instead of body, fix font issue on vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,12 +55,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
-          fontSans.variable
-        )}
+    <html lang="en" suppressHydrationWarning className={fontSans.variable}>
+    <body
+      className={cn(
+        "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
+      )}
       >
         <ThemeProvider attribute="class" defaultTheme="light">
           <TooltipProvider delayDuration={0}>
